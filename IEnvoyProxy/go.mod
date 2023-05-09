a22bbea91f57dab3e11df93c40f7930ea8b6ff76
Steven McDonald <steven.mcdonald@gmail.com>
2023-05-09 12:19:08 -0700

    This is pretty much just copy/paste of the snowflake code from IPtProxy 2.0.0; I also copied over their code for building in a temp directory, since Go seems to leak that path of the build system.

--- IEnvoyProxy/go.mod
+++ IEnvoyProxy/go.mod
@@ -11,15 +11,12 @@ replace (
 )
 
 require (
+	git.torproject.org/pluggable-transports/snowflake.git/v2 v2.0.0-00010101000000-000000000000
 	github.com/lucas-clemente/quic-go v0.27.2 // indirect
-	github.com/onsi/ginkgo v1.16.5 // indirect
-	github.com/onsi/gomega v1.17.0 // indirect
-	github.com/pion/dtls/v2 v2.1.5 // indirect
-	github.com/pion/sctp v1.8.2 // indirect
-	github.com/pion/transport v0.13.0 // indirect
 	github.com/tobyxdd/hysteria v1.0.5
 	github.com/v2fly/v2ray-core v4.15.0+incompatible
 	github.com/v2fly/v2ray-core/v5 v5.0.7 // indirect
-	golang.org/x/crypto v0.0.0-20220516162934-403b01795ae8 // indirect
-	golang.org/x/mobile v0.0.0-20221110043201-43a038452099 // indirect
+	golang.org/x/mobile v0.0.0-20230427221453-e8d11dd0ba41 // indirect
+	golang.org/x/sync v0.0.0-20220819030929-7fc1605a5dde // indirect
+	golang.org/x/tools v0.1.12 // indirect
 )
--- build.sh
+++ build.sh
@@ -1,9 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
 set -euo pipefail
 
+set -x
+
 TARGET=ios,iossimulator,macos
 OUTPUT=IEnvoyProxy.xcframework
+TEMPDIR="${TEMPDIR:/tmp/}"
+TEMPDIR="${TMPDIR}IEnvoyProxy"
+
+echo "TEMPDIR: ${TEMPDIR}"
 
 if test "$1" = "android"; then
   TARGET=android
@@ -22,41 +28,63 @@ printf '\n--- Golang 1.16 or up needs to be installed! Try "brew install go" on 
 printf '\n--- Installing gomobile...\n'
 go install golang.org/x/mobile/cmd/gomobile@latest
 
+# Prepare build environment
+# Go leaks the build path in to the binary, so use a temp dir to build
+# based on https://github.com/tladesignz/IPtProxy/pull/38
+printf '\n\n--- Prepare build environment at %s...\n' "$TEMPDIR"
+CURRENT=$PWD
+rm -rf "$TEMPDIR" || true
+mkdir -p "$TEMPDIR"
+cp -a IEnvoyProxy "$TEMPDIR/"
+
 # Fetch submodules.
 printf '\n\n--- Fetching submodule dependencies...\n'
 if test -e ".git"; then
     # There's a .git directory - we must be in the development pod.
+    git submodule update --init --recursive
     cd hysteria || exit 1
     git reset --hard
+    cp -a . "$TEMPDIR/hysteria"
     cd ../v2ray-core || exit 1
     git reset --hard
-    git clean -ffd
+    git clean -ffd # we add a file
+    cp -a . "$TEMPDIR/v2ray-core"
+    cd ../snowflake || exit 1
+    git reset --hard
+    cp -a . "$TEMPDIR/snowflake"
     cd ..
 else
     # No .git directory - That's a normal install.
-    git clone https://github.com/HyNetwork/hysteria.git
+    git clone https://github.com/HyNetwork/hysteria.git "$TEMPDIR/hysteria"
     cd hysteria || exit 1
     git checkout --force --quiet da16c88
     cd ..
-    git clone https://github.com/v2fly/v2ray-core.git
+    git clone https://github.com/v2fly/v2ray-core.git "$TEMPDIR/v2ray-core"
     cd v2ray-core || exit 1
     git checkout --force --quiet b4069f74
     cd ..
+    git clone https://git.torproject.org/pluggable-transports/snowflake.git "$TEMPDIR/snowflake"
+    cd "$TEMPDIR/snowflake" || exit 1
+    git checkout --force --quiet 7b77001
+    cd "$CURRENT" || exit 1
 fi
 
 # Apply patches.
 printf '\n\n--- Apply patches to submodules...\n'
 echo `pwd`
-patch --directory=hysteria --strip=1 < hysteria.patch
-patch --directory=v2ray-core --strip=1 < v2ray-core.patch
+patch --directory=$TEMPDIR/hysteria --strip=1 < hysteria.patch
+patch --directory=$TEMPDIR/v2ray-core --strip=1 < v2ray-core.patch
+patch --directory=$TEMPDIR/snowflake --strip=1 < snowflake.patch
 
 # Compile framework.
 printf '\n\n--- Compile %s...\n' "$OUTPUT"
 export PATH=~/go/bin:$PATH
-cd IEnvoyProxy || exit 1
+cd "${TEMPDIR}/IEnvoyProxy" || exit 1
 
 gomobile init
 
-gomobile bind -target=$TARGET -o ../$OUTPUT -iosversion 11.0 -androidapi 19 -v
+MACOSX_DEPLOYMENT_TARGET=11.0 gomobile bind -target=$TARGET -o $CURRENT/$OUTPUT -iosversion=11.0 -androidapi=19 -v -tags=netcgo -trimpath
+
+rm -rf "$TEMPDIR"
 
 printf '\n\n--- Done.\n\n'
